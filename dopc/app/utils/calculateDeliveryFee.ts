f733cedf936d87107ab432e45e89fc89139f17fd
TFT <artoreinik@gmail.com>
2025-01-25 11:36:11 +0200

    fixed delivery fee calculation bug

--- dopc/app/utils/calculateDeliveryFee.ts
+++ dopc/app/utils/calculateDeliveryFee.ts
@@ -1,22 +1,27 @@
-export function calculateDeliveryFee(distance: number, setDeliveryFee: (fee: number) => void, distanceRanges: { min: number; max: number; a: number; b: number }[], deliveryBaseFee: number) {
-        let fee = 0;
-        distanceRanges.forEach((range) => {
-            if (range.max !== 0) {
-                if (distance >= range.min && distance < range.max) {
-                    fee =
-                        deliveryBaseFee +
-                        range.a +
-                        Math.round((range.b * distance) / 10);
-                }
-            } else {
-                if (distance < range.min) {
-                    fee =
-                        deliveryBaseFee +
-                        range.a +
-                        Math.round((range.b * distance) / 10);
-                }
+export function calculateDeliveryFee(
+    distance: number,
+    setDeliveryFee: (fee: number) => void,
+    distanceRanges: { min: number; max: number; a: number; b: number }[],
+    deliveryBaseFee: number
+) {
+    let fee = 0;
+    distanceRanges.forEach((range) => {
+        if (range.max !== 0) {
+            if (distance >= range.min && distance < range.max) {
+                fee =
+                    deliveryBaseFee +
+                    range.a +
+                    Math.round((range.b * distance) / 10);
             }
-        });
-        setDeliveryFee(fee / 100);
-        return fee / 100;
-    }
+        } else {
+            if (distance < range.min && fee === 0) {
+                fee =
+                    deliveryBaseFee +
+                    range.a +
+                    Math.round((range.b * distance) / 10);
+            }
+        }
+    });
+    setDeliveryFee(fee / 100);
+    return fee / 100;
+}
